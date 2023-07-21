31f5d9eb3f2548934e53a499b281d5a2dd9e245a
Pagnavathcoding <88227695+Pagnavathcoding@users.noreply.github.com>
2023-07-21 21:44:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 // Database Connection
-mongoose.connect(process.env.DATABASE).then(() => {
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("Database Connected!");
 }).catch(err => console.log(err));
 
